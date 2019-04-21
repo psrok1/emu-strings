f875a15121e49d98a88ef623892efdfee8db056b
psrok1 <psrok1@gmail.com>
2019-04-21 18:17:41 +0200

    Update .gitignore

--- test.js
+++
@@ -1,7 +0,0 @@
-WScript.Quit(1)
-var shapp = new ActiveXObject('Shell.Application');
-var startup = shapp['NameSpace'](7);
-WScript.echo("======DEBUG=======");
-WScript.echo(typeof startup);
-WScript.echo(typeof startup.Self);
-WScript.echo("======DEBUG=======");
--- test.sh
+++
@@ -1,5 +0,0 @@
-curl \
-    -F "engine=JScript" \
-    -F "emulator=WinedropEmulator" \
-    -F "file=@test.js" \
-    http://localhost:5000/api/submit
