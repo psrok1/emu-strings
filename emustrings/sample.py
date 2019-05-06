9e190ad7c304e5e3f1922a0620baad50166e76d9
Pawel Srokosz <pawel.srokosz@cert.pl>
2019-05-06 22:15:33 +0200

    Fixed decoding with chardet detection

--- emustrings/sample.py
+++ emustrings/sample.py
@@ -10,7 +10,8 @@ class Sample(object):
         self.sha256 = hashlib.sha256(code).hexdigest()
         self.name = name or self.sha256
         try:
-            self.str_code = code.decode(chardet.detect(code))
+            encoding = chardet.detect(code)["encoding"]
+            self.str_code = code.decode(encoding or "utf8")
         except Exception:
             self.str_code = ""
         self.icase_str_code = self.str_code.lower()
