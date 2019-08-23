506bcd7dd2c3edf9715b2b2dc7d8ff337a9d6dcb
psrok1 <psrok1@gmail.com>
2019-08-23 18:39:01 +0200

    Support for custom Dockerfile name

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
 
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 # Pull branch build
@@ -5,6 +6,10 @@ if ! ( docker pull "${IMAGE_NAME}:${BRANCH}" ); then
     # Pull latest if branch doesn't exist yet
     BRANCH="latest"
     docker pull "${IMAGE_NAME}:${BRANCH}" || true
+fi
+
+if [[ ! -d "$DOCKERFILE "]]; then
+    DOCKERFILE="${DOCKERFILE_PATH}/Dockerfile"
 fi
 
 docker build --pull --cache-from "${IMAGE_NAME}:${BRANCH}" --tag "$IMAGE_NAME" "$DOCKERFILE_PATH"
