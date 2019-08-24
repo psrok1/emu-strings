6fb67baa4f17a268b81e2ccbd8c1604cf90fb92a
Paweł Srokosz <psrok1@gmail.com>
2019-08-24 18:25:29 +0200

    Added basic Travis-based CI (#2)

---
+++ .travis/script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+
+# Pull branch build
+if ! ( docker pull "${IMAGE_NAME}:${BRANCH}" ); then 
+    # Pull latest if branch doesn't exist yet
+    BRANCH="latest"
+    if ! ( docker pull "${IMAGE_NAME}:${BRANCH}" ); then
+        echo "Previous version not found - building from scratch"
+    else
+        echo "Pulled ${IMAGE_NAME}:${BRANCH}"
+    fi
+else
+    echo "Pulled ${IMAGE_NAME}:${BRANCH}"
+fi
+
+if [ -z "$DOCKERFILE" ]; then
+    DOCKERFILE="${DOCKERFILE_PATH}/Dockerfile"
+fi
+
+echo "Building ${IMAGE_NAME}:${BRANCH} using $DOCKERFILE"
+
+docker build --pull --cache-from "${IMAGE_NAME}:${BRANCH}" --tag "$IMAGE_NAME" --file "$DOCKERFILE" "$DOCKERFILE_PATH"
