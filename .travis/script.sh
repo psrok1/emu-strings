3d0c775bc320ca897cd1fc3729bc02789c1754a0
psrok1 <psrok1@gmail.com>
2019-08-24 18:00:00 +0200

    Added debug messages

--- .travis/script.sh
+++ .travis/script.sh
@@ -5,11 +5,19 @@ BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then echo $TRAVIS_BRANCH; else
 if ! ( docker pull "${IMAGE_NAME}:${BRANCH}" ); then 
     # Pull latest if branch doesn't exist yet
     BRANCH="latest"
-    docker pull "${IMAGE_NAME}:${BRANCH}" || true
+    if ! ( docker pull "${IMAGE_NAME}:${BRANCH}" ); then
+        echo "Previous version not found - building from scratch"
+    else
+        echo "Pulled ${IMAGE_NAME}:${BRANCH}"
+    fi
+else
+    echo "Pulled ${IMAGE_NAME}:${BRANCH}"
 fi
 
 if [ -z "$DOCKERFILE" ]; then
     DOCKERFILE="${DOCKERFILE_PATH}/Dockerfile"
 fi
+
+echo "Building ${IMAGE_NAME}:${BRANCH} using $DOCKERFILE"
 
 docker build --pull --cache-from "${IMAGE_NAME}:${BRANCH}" --tag "$IMAGE_NAME" --file "$DOCKERFILE" "$DOCKERFILE_PATH"
