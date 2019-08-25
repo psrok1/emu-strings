a46a2320e80a74b8fea5370315605a8048e3c812
psrok1 <psrok1@gmail.com>
2019-08-25 02:33:03 +0200

    Added publish stage, fixed test script

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,7 +3,7 @@
 export TAG
 TAG=$(if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
-pip install tests/requirements.txt
+pip install -r tests/requirements.txt
 
 docker-compose pull && \
 docker-compose up -d && \
